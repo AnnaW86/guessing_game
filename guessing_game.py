a69759a58939f3181c4f8b50f76247366ff12660
AnnaW86 <annawebbstewart@gmail.com>
2022-03-08 12:05:51 +0000

    updated for all rps options

--- guessing_game.py
+++ guessing_game.py
@@ -1,9 +1,15 @@
+from http.cookiejar import DefaultCookiePolicy
 from multiprocessing import RLock
 
 
 userInput = input("Choose rock, paper or scissors:").lower()
+defaultInput = "rock"
 
-if userInput == "rock":
-    print("You rock")
+if userInput == defaultInput:
+    print("Ah, it's a draw")
+elif userInput == "paper":
+    print("Paper beats rock - you win!")
+elif userInput == "scissors":
+    print("Aha, rock beats scissors - you lose!")
 else:
-    print("No you don't")
+    print("Well that wasn't one of the options.")
